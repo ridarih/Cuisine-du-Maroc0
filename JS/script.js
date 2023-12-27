6aee293dd3e9311aba3c451f59e61ed58c9722d1
ridarih <rihirida6@gmail.com>
2023-12-27 13:21:35 +0100

    first commit

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+/* 
+##############################
+#          Regalar           #
+##############################
+*/
+/* let number = "123456979os426ri4da"
+let numberRe =/[^1-9]/ig
+console.log(number.match(numberRe)) */
---
+++ README.md
@@ -0,0 +1,1 @@
+# Cuisine-du-Maroc0
